ffb43447c58ef7cd9fc092e58c267ed6aeb0bffd
Zamith <zamith.28@gmail.com>
2014-06-20 18:51:05 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TddExample::Application.config.secret_key_base = '9090df085e584f64e2ef2a1a69822885613cccafd4a4f542ed87adb762e3246cc1270c93d555812beafb7fa4f45c8c19b6908bc90607341e5d500859c722f8b3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TddExample::Application.config.session_store :cookie_store, key: '_tdd-example_session'
